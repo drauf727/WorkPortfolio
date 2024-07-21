a82850b7869fcf5270315fcc1ed31dac6772631b
David Rauf <davidrauf@gmail.com>
2024-07-21 13:26:50 -0400

    about me and contact page working

--- David's Portfolio/src/pages/About.jsx
+++ David's Portfolio/src/pages/About.jsx
@@ -1,17 +1,20 @@
+import profilePicture from '../assets/dave.png'
+
 export default function About() {
     return (
-      <div>
-        <h1>About Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, numquam dolores dicta, illo possimus ipsa sint necessitatibus vero asperiores aperiam nobis nemo tempora expedita impedit? Accusantium voluptatem quibusdam in doloremque ad id labore? Eum, nemo molestias ipsam praesentium minus aut, obcaecati autem explicabo veritatis sapiente animi aspernatur porro repudiandae at aperiam voluptatem voluptate voluptatum voluptas maiores, ratione unde consequuntur reiciendis harum tempora! Quos ab expedita aliquam quod labore. Voluptatum fugit veniam illum placeat, totam explicabo assumenda eum quidem recusandae! Rerum similique sunt omnis quia recusandae, maiores debitis quidem earum quasi possimus ipsa a, at deleniti enim distinctio maxime delectus?
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse iure quasi dolore nam debitis id ut consequuntur unde molestias officiis sunt perspiciatis a impedit quaerat pariatur adipisci, reprehenderit, tenetur nemo autem! Modi accusantium voluptatem aliquam molestias cumque nisi, culpa maxime quod accusamus porro eligendi. Itaque fugit excepturi a voluptas. Velit, inventore adipisci sit, quibusdam vitae magni eligendi nobis, cum asperiores dolorum eos ipsa. Officiis molestias ipsam aspernatur modi id delectus quos necessitatibus inventore distinctio consequuntur? Sit ratione totam ea fugiat atque nemo, voluptatem sequi incidunt animi non consequatur cumque culpa mollitia numquam error omnis, debitis reiciendis repellat nam. Cupiditate quia unde ratione. Minima aperiam nesciunt repellendus tenetur unde magnam! Odio neque exercitationem nesciunt nostrum voluptatum eveniet nulla, aspernatur ex odit, eligendi ut! Consequuntur beatae odio nobis error vitae, ea eius expedita soluta, rem odit nulla? Repellendus a voluptatum deserunt aliquid reiciendis atque eligendi? Adipisci debitis qui quibusdam quos et dolor deleniti ullam nulla provident minus veniam ab nihil sequi, maiores iste quis totam consectetur unde voluptate praesentium voluptates. Alias asperiores at odit quis excepturi repudiandae neque praesentium aperiam, esse consectetur, iure suscipit hic tenetur quibusdam quae laboriosam dolorem facere eveniet consequatur, aliquid deleniti. Ratione sapiente eum modi obcaecati tempora consequuntur, numquam aperiam cupiditate. Quaerat labore quibusdam dolore nam quos nihil at molestiae, itaque minima maiores nobis dolores non pariatur quae cupiditate reiciendis obcaecati vel. Provident dignissimos assumenda officiis alias odit ad nam exercitationem explicabo ipsam nesciunt? Veritatis soluta voluptatem amet nesciunt, tempora eius quas fuga incidunt non impedit! Provident cum a sint quidem, doloribus assumenda sunt repellendus atque fugit debitis odio iusto maiores exercitationem porro ipsam aut eos quibusdam deleniti molestiae quae totam? Eos veritatis deleniti error quasi, maxime, repellendus repudiandae voluptatum reiciendis quos vel non. Soluta dolorum facilis temporibus corrupti possimus aspernatur. Natus fugiat vitae neque unde sunt sequi molestiae delectus. Cupiditate amet suscipit vel perspiciatis reiciendis illo maxime placeat, alias, aut debitis reprehenderit quam! Ab sit accusamus blanditiis iste dicta, fugit nemo quis sapiente nobis id, repellat libero quam, quibusdam voluptatem nostrum architecto alias provident autem corrupti mollitia. Rerum sequi fuga quos dolorum labore qui reprehenderit? Soluta dignissimos iste obcaecati est sunt minus. Voluptates et aliquid suscipit quaerat sed esse ex tenetur omnis quia? Repellat voluptates numquam neque repudiandae iure autem ut voluptas eos tempora. Accusamus sed tenetur suscipit saepe expedita, maiores est facere voluptatum a aliquid. Eaque ducimus veniam, amet facilis omnis ipsa nam quam voluptates aliquam fugiat ut autem, est, quasi nulla libero debitis! Ipsam, dolor minus aspernatur sapiente ea quas facilis alias nemo est quasi error perferendis voluptatibus reiciendis repudiandae dolore soluta perspiciatis. Suscipit sed assumenda est dolores expedita molestias consequuntur consequatur velit veniam blanditiis nihil sunt maxime, distinctio quos provident, voluptates aperiam. Aperiam maiores explicabo delectus sapiente error sint deleniti nulla culpa commodi, eveniet sunt! Velit at vero, aperiam quo, fuga quam in, excepturi accusamus sunt quaerat adipisci laboriosam similique dignissimos. Architecto nam quod iusto libero unde aliquid. Explicabo error assumenda nesciunt neque beatae deleniti soluta. Minima esse laborum voluptatum itaque a? Autem, nostrum?
-        </p>
+      <>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      {/* <h1 className="text-center">About Me</h1> */}
+      <img src={profilePicture}alt="Profile" style={{ width: '150px', height: '150px', borderRadius: '20%', marginBottom: '20px' }} />
+      <div className="text-wrap" style={{ width: '30rem', textAlign: 'justify' }}>
+        Thank you for coming by and checking out my portfolio page. My name is David and I am a software engineer from
+        St. Petersburg Florida. I am specializing in the MERN programming stack with a focus on web devolpement for retail
+        business. Please take some time to view some of my work using the portfolio link above and if you like my work
+        or have any questions please use the contact tab to reach out to me directly. There is also an attached resume that
+        you can download in the links above.
       </div>
+    </div>
+      </>
     );
   }
   
--- David's Portfolio/src/pages/Contact.jsx
+++ David's Portfolio/src/pages/Contact.jsx
@@ -1,16 +1,46 @@
+import { useState } from 'react';
+
 export default function Contact() {
-    return (
-      <div>
-        <h1>About Page</h1>
-        <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-        </p>
-      </div>
-    );
+  const blankForm = {
+    name: '',
+    email: '',
+    message: ''
   }
-  
+
+  const [formData, setFormData] = useState(blankForm);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form data submitted:', formData);
+    setFormData(blankForm)
+  };
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <h1>Contact Me</h1>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', alignItems: 'justify', gap: '20px' }}>
+        <label>
+          Name:
+          <input className='ms-2' type="text" name="name" value={formData.name} onChange={handleChange} required />
+        </label>
+        <label>
+          Email:
+          <input className='ms-2' type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </label>
+        <label>
+          Message:
+          <textarea className='ms-2' name="message" value={formData.message} onChange={handleChange} required />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
